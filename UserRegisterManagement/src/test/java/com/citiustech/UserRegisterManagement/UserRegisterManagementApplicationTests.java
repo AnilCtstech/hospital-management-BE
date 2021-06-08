96ec58fa39fa13dfa9236da3ee17a08276734948
PushpakB-CTzen <Pushpak.Bhambar@citiustech.com>
2021-06-08 15:05:48 +0530

    Updated Setup

---
+++ UserRegisterManagement/src/test/java/com/citiustech/UserRegisterManagement/UserRegisterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citiustech.UserRegisterManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegisterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
