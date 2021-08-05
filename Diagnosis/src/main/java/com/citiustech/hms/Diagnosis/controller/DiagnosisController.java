436eec414519a9b0be1ae2e6fd582719e74b6355
PushpakB-CTzen <Pushpak.Bhambar@citiustech.com>
2021-08-05 17:59:30 +0530

    get diagnosis for patient

--- Diagnosis/src/main/java/com/citiustech/hms/Diagnosis/controller/DiagnosisController.java
+++ Diagnosis/src/main/java/com/citiustech/hms/Diagnosis/controller/DiagnosisController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -47,5 +48,14 @@ public class DiagnosisController {
 	return new ResponseEntity<List<Long>>(patientIdList, HttpStatus.NO_CONTENT);
 	}
 	
+	@GetMapping("/patients/{patientId}")
+	public ResponseEntity<List<DiagnosisDto>> getDiagnosisByPatient(@RequestHeader("authorization") String authorization,@PathVariable String patientId ){
+	List<DiagnosisDto> patientIdList=diagnosisService.getDiagnosisByPatient(authorization,patientId);
+	if(patientIdList!=null) {
+	return new ResponseEntity<List<DiagnosisDto>>(patientIdList, HttpStatus.OK);
 
+	 }
+	return new ResponseEntity<List<DiagnosisDto>>(patientIdList, HttpStatus.NO_CONTENT);
+	}
+	
 }
